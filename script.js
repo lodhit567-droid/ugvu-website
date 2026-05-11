73d368512ff1a206967b899a3755470aa1f2a243
Tarun lodhi <lodhit567@gmail.com>
2026-05-11 20:55:28 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 let btn= document.querySelector("button");
-let main = document.querySelector("main");
-
-btn.addEventListener("click", function(){
-   let x = Math.random() * 100;
-   let y = Math.random() * 100;
-   let c = Math.floor(Math.random() * 256);
-   let c1 = Math.floor(Math.random() * 256);
-   let c2 = Math.floor(Math.random() * 256);
-   let div = document.createElement("div")
-   div.style.height = "100px";
-   div.style.width = "100px";
-   div.style.position = "absolute";
-   div.style.rotate = `${Math.random() * 360}deg`;
-   div.style.backgroundColor = `rgb(${c}, ${c1}, ${c2})`;
-   div.style.margin = "10px";
-   div.style.left = x + "%";
-   div.style.top = y + "%";
-
-   main.appendChild(div);
-});
+let inner= document.querySelector(".inner");
+let h1= document.querySelector("h1");
+let grow = 0;
+btn.addEventListener("click",()=>{
+   btn.style.pointerEvents = "none";
+   let a = setInterval(()=>{
+   grow++;
+   h1.innerHTML= grow+"%";
+   inner.style.width= grow+"%";
+},50)
+setTimeout(() => {
+   clearInterval(a);
+   h1.innerHTML= "Download Complete";
+   btn.style.opacity = 0.1;
+}, 5000);
+})
